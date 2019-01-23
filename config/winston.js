6d73846aca9b2c1d98001b9c305482ffa5c00b3d
Parthiv Kativarapu <Parthiv.Kativarapu@IDeaS.com>
2019-01-23 13:37:30 +0530

    Trying out something

--- config/winston.js
+++ config/winston.js
@@ -1,13 +1,29 @@
 const winston = require('winston');
 
+var options = {
+    console: {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true
+    }
+};
+
 const logger = winston.createLogger({
-    level: 'info',
+    level: 'debug',
     format: winston.format.json(),
     defaultMedia: { service: 'user-service' },
     transports: [
-        new winston.transports.Console()
-    ]
+        new winston.transports.Console(options.console)
+    ],
+    exitOnError: false
 });
+
+logger.stream = {
+    write: function(message, encoding) {
+        logger.info(message);
+    }
+}
 
 if (process.env.NODE_ENV !== 'production') {
     logger.add(new winston.transports.Console({
--- index.js
+++ index.js
@@ -4,16 +4,35 @@ const express = require('express'),
     morgan = require('morgan'),
     logger = require('./config/winston'),
     session = require('express-session'),
+    FileStore = require('session-file-store')(session),
+    bodyParser = require('body-parser'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
     mongodb = require('mongodb');
 
 const port = process.env.PORT || 8080;
 
-app.use(morgan('combined', { stream: logger.stream }));
+passport.use(new LocalStrategy(
+    { usernameField: 'email' },
+    (email, password, done) => {
+        logger.info( {message: 'Local Strategy'});
+        const user = users[0]
+        if(email == user.email && password == user.password) {
+            logger.info({ message: 'localstrat retrun true' })
+            return done(null, user);
+        }
+    }
+))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('combined', { "stream": logger.stream }));
 app.use(session({
     genid: (req) => {
-        console.log(req.sessionID)
+        logger.info(req.sessionID)
         return uuid()
     },
+    store: new FileStore(),
     secret: 'secret',
     resave: false,
     saveUninitialized: true
@@ -22,6 +41,20 @@ app.use(session({
 app.get('/', (req, res) => {
     logger.info({message: req.sessionID});
     res.send('Homepage')
+})
+
+app.get('/login', (req, res) => {
+    logger.info({ id: req.sessionID })
+    passport.authenticate('local', (err, user, info) => {
+        logger.info({ message: 'in login in POST' });
+        req.login(user, (err) => {
+            logger.info({ passport: `${JSON.stringify(req.session.passport )}`})
+        })
+    })
+})
+
+app.post('/login', (req, res) => {
+    logger.info({ id: req.body })
 })
 
 app.listen(port, () => {
